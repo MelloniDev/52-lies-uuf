e38d78426a9175d40da6dc764861a5ab63a11509
Nils Hollenstein <nils.hollenstein@gmail.com>
2025-04-07 13:32:00 +0200

    Added player class and logic extended

--- src/app/logic.js
+++ src/app/logic.js
@@ -1,12 +1,30 @@
 import { Rethink_Sans } from "next/font/google";
 import "./card";
+import { card } from "./card";
+import { act } from "react";
 
 let stack = [{ image: "tset", value: "ACE", suit: "SPADES" }];
 class logic {
-  allowedToStack(currentCard, newCard) {
-    if (newCard.value === "Jack" && currentCard.value !== "JACK") return true;
+  cardIsPlayed(playedCard, stackCard, activePlayer, otherPlayer) {
+    if (!this.allowedToStack(stackCard, playedCard)) return false; // If turn is not allowed
 
-    if (currentCard.value != newCard.value && currentCard.suit != newCard.suit)
+    let check = this.checkForSpecialCard(playedCard);
+    if (check === 1) {
+      otherPlayer.drawCards(2); // Placeholder
+    } else if (check === 2) {
+      return activePlayer.playerNumber; // Gibt Spielernummer zurück von dem Spieler der am Zug ist
+    } else if (check === 3) {
+      // JACK-Logik noch einbauen
+      return otherPlayer.playerNumber;
+    } else if (check === 4) {
+      return activePlayer.playerNumber;
+    } else if (check === 0) return otherPlayer.playerNumber;
+  }
+
+  allowedToStack(stackCard, newCard) {
+    if (newCard.value === "Jack" && stackCard.value === "JACK") return false;
+
+    if (stackCard.value != newCard.value && stackCard.suit != newCard.suit)
       return false;
 
     return true;
@@ -25,9 +43,5 @@ class logic {
       default:
         return 0;
     }
-  }
-
-  activateSpecialCard(newCard, stack) {
-    let specialValue = this.checkForSpecialCard(newCard);
   }
 }
---
+++ src/app/player.js
@@ -0,0 +1,5 @@
+import { card } from "./card";
+class player {
+  playerNumber; // Has to be set on start
+  cards = []; // List of Card-Objects
+}
