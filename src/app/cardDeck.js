44f4b9ec22a72b671a4dfe0e9dea636b658e5a1a
MelloniDev <>
2025-04-14 11:48:09 +0200

    created getCards function

--- src/app/cardDeck.js
+++ src/app/cardDeck.js
@@ -18,10 +18,32 @@ function createCardDeck() {
     
 }
 
-module.exports = createCardDeck;
+function getCards(deckID) {
+ 
+    return new Promise((resolve, reject)=>{
+       (async ()=>{
+        
+            let req = await fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=52`);
+
+            //console.log(req);
+
+            let res = await req.json();
+
+             
+            resolve(res.cards);
+        })(); 
+    })
+}
+
+
+module.exports = { createCardDeck, getCards };
 
 // Testing
 
-let CardDeck = new createCardDeck().then((deckID)=>{
+new createCardDeck().then((deckID)=>{
     console.log(deckID);
+
+    new getCards(deckID).then(cards => {
+        console.log(cards);
+    });
 });
