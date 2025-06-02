9fa7aa6a7702d459927aea02389580c828d9bc5e
Nils Hollenstein <nils.hollenstein@gmail.com>
2025-06-02 13:06:56 +0200

    Ajusted Names of Variables and Functions

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import DeckScatter from "./components/DisplayDeckService";
+import DisplayDeck from "./components/DisplayDeckService";
 
 export default function Home() {
   return (
     <main>
-      <DeckScatter />
+      <DisplayDeck />
     </main>
   );
 }
